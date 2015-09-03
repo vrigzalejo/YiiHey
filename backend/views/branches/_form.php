6ddfdae4769c5f421ec6e365c09f82d2362ede36
Vrigz Alejo <nuedu2012@gmail.com>
2015-09-03 08:06:04 +0100

    [L31] Submitting Forms Using Ajax (undone)

--- backend/views/branches/_form.php
+++ backend/views/branches/_form.php
@@ -11,29 +11,55 @@ use yii\widgets\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="branches-form">
+    <div class="branches-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+        <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'companies_company_id')->widget(Select2::classname(), [
-        'data' => ArrayHelper::map(Companies::find()->all(), 'company_id', 'company_name'),
-        'language' => 'de',
-        'options' => ['placeholder' => 'Select a state ...'],
-        'pluginOptions' => [
-            'allowClear' => true
-        ],
-    ]); ?>
+        <?= $form->field($model, 'companies_company_id')->widget(Select2::classname(), [
+            'data' => ArrayHelper::map(Companies::find()->all(), 'company_id', 'company_name'),
+            'language' => 'de',
+            'options' => ['placeholder' => 'Select a state ...'],
+            'pluginOptions' => [
+                'allowClear' => true
+            ],
+        ]); ?>
 
-    <?= $form->field($model, 'branch_name')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'branch_name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'branch_address')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'branch_address')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'branch_status')->dropDownList(['active' => 'Active', 'inactive' => 'Inactive',], ['prompt' => 'Status']) ?>
+        <?= $form->field($model, 'branch_status')->dropDownList(['active' => 'Active', 'inactive' => 'Inactive',], ['prompt' => 'Status']) ?>
 
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <div class="form-group">
+            <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        </div>
+
+        <?php ActiveForm::end(); ?>
+
     </div>
+<?php $script
+    = <<< JS
+// here you right all your javascript
+$('form#{$model->formName()}').on('beforeSubmit', function(e) {
+    var \$form = $(this);
+    $.post(
+        \$form.attr('action'), // serialize Yii2 form
+        \$form.serialize()
+    )
+        .done(function(result) {
+        /*if(result.message == 'Success') {
+            $(document).find('#secondmodal').modal('hide');
+            $.pjax.reload({container: '#commodity-grid'});
+        } else {
+            $(\$form).trigger('reset');
+            $('#message').html(result.message);
+        }*/
+        }).fail(function() {
+            console.log('server error');
+        });
+    return false;
+});
 
-    <?php ActiveForm::end(); ?>
-
-</div>
+JS;
+$this->registerJs($script);
+?>
