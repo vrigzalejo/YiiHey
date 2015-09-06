b1e4a1b63405372775c313021eef7939e57fc978
Vrigz Alejo <nuedu2012@gmail.com>
2015-09-06 20:23:12 +0800

    Added fixtures; Fixed auth_assignment migrations;

---
+++ tests/unit/fixtures/data/auth_assignment.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    [
+        'item_name' => 'admin',
+        'user_id' => 1,
+        'created_at' => '2015-08-31 04:33:39',
+    ],
+    [
+        'item_name' => 'admin',
+        'user_id' => 2,
+        'created_at' => '2015-09-02 18:29:23',
+    ],
+    [
+        'item_name' => 'create-branch',
+        'user_id' => 3,
+        'created_at' => '2015-09-04 02:39:01',
+    ],
+    [
+        'item_name' => 'admin',
+        'user_id' => 4,
+        'created_at' => '2015-09-01 17:09:08',
+    ],
+    [
+        'item_name' => 'create-company',
+        'user_id' => 5,
+        'created_at' => '2015-09-02 00:11:30',
+    ],
+    [
+        'item_name' => 'admin',
+        'user_id' => 6,
+        'created_at' => '2015-09-02 10:21:16',
+    ],
+    [
+        'item_name' => 'admin',
+        'user_id' => 7,
+        'created_at' => '2015-09-02 22:31:11',
+    ],
+    [
+        'item_name' => 'admin',
+        'user_id' => 8,
+        'created_at' => '2015-09-04 17:05:37',
+    ],
+    [
+        'item_name' => 'create-branch',
+        'user_id' => 9,
+        'created_at' => '2015-09-01 22:05:18',
+    ],
+    [
+        'item_name' => 'create-branch',
+        'user_id' => 10,
+        'created_at' => '2015-09-05 23:27:34',
+    ],
+];
