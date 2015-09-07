73935ceaf499f5bd73aeae249d43c756f1db0539
Vrigz Alejo <nuedu2012@gmail.com>
2015-09-07 23:19:48 +0800

    [L41] Login Page using the template property

--- backend/assets/LoginAsset.php
+++ backend/assets/LoginAsset.php
@@ -15,11 +15,25 @@ class LoginAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl  = '@web';
-    public $css      = [ ];
-    public $js       = [ ];
-    public $depends
-                     = [
-            'yii\web\YiiAsset',
-            'yii\bootstrap\BootstrapAsset',
-        ];
+    public $css      = [
+        '//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css',
+        '//maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css',
+        'css/AdminLTE.min.css',
+        'plugins/iCheck/flat/blue.css',
+    ];
+    public $js       = [
+        '//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js',
+        '//code.jquery.com/ui/1.11.4/jquery-ui.min.js',
+        '//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js',
+        'plugins/sparkline/jquery.sparkline.min.js',
+        'plugins/slimScroll/jquery.slimscroll.min.js',
+        'plugins/fastclick/fastclick.min.js',
+        'js/app.min.js',
+        'js/dashboard.js',
+        'js/main.js',
+    ];
+    public $depends  = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
 }
--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -7,27 +7,37 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
-$this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
+$this->title                     = 'Login';
+$this->params[ 'breadcrumbs' ][] = $this->title;
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="login-box">
+        <div class="login-logo">
+            <a href="#"><b>Admin</b>LTE</a>
+        </div>
+        <div class="login-body">
+            <p class="login-box-msg">Sign in to start your session</p>
+            <?php $form = ActiveForm::begin( [ 'id' => 'login-form' ] ); ?>
 
-    <p>Please fill out the following fields to login:</p>
+            <?= $form->field( $model, 'username',
+                [
+                    'options'  => [ 'tag' => 'div', 'class' => 'form-group field-loginform-username has-feedback required', ],
+                    'template' => '{input}<span class="glyphicon glyphicon-user form-control-feedback"></span>{error}{hint}',
+                ] )->textInput( [ 'placeholder' => 'Username' ] )
+            ?>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+            <?= $form->field( $model, 'password',
+                [
+                    'options'  => [ 'tag' => 'div', 'class' => 'form-group field-loginform-password has-feedback required', ],
+                    'template' => '{input}<span class="glyphicon glyphicon-lock form-control-feedback"></span>{error}{hint}',
+                ] )->passwordInput( [ 'placeholder' => 'Password' ] )
+            ?>
 
-                <?= $form->field($model, 'username') ?>
-
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
+            <?= $form->field( $model, 'rememberMe' )->checkbox()
+            ?>
+            <div class="form-group">
+                <?= Html::submitButton( 'Login', [ 'class' => 'btn btn-primary', 'name' => 'login-button' ] ) ?>
+            </div>
 
             <?php ActiveForm::end(); ?>
         </div>
